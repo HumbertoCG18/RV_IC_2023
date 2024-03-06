f8f03be91ec2c165fd6a50558241a390df86f56d
Humberto Corrêa Gomes <humberto.gomes002@edu.pucrs.br>
2024-03-06 14:46:27 -0300

    update scriots

--- Assets/Scripts/BalancaCompleta.cs
+++ Assets/Scripts/BalancaCompleta.cs
@@ -7,11 +7,24 @@ public class BalancaCompleta : MonoBehaviour
     public GameObject haste;
     public GameObject meioHaste;
     public GameObject meioSuporte;
+    public GameObject suporteConectadoEsquerda;
+    public GameObject suporteConectadoDireita;
+    public GameObject pontoEsquerda;
+    public GameObject pontoDireita;
+
+    private HingeJoint hingePratoEsquerda;
+    private HingeJoint hingePratoDireita;
+    private FixedJoint fixedSuporteEsquerdo;
+    private FixedJoint fixedSuporteDireito;
+    private HingeJoint hingeHaste;
 
     void Start()
     {
         // Conectar os pratos aos seus respectivos suportes e pontos na haste
         ConectarPratos();
+
+        // Conectar a haste ao meio suporte
+        ConectarHaste();
 
         // Adicionar as joints aos pratos e suportes
         AdicionarJoints();
@@ -19,55 +32,75 @@ public class BalancaCompleta : MonoBehaviour
 
     void ConectarPratos()
     {
-        // Conectar o Prato Esquerda ao seu respectivo suporte e ponto na haste
-        Transform suporteEsquerda = transform.Find("X(Prato Esquerda)/suporteConectado_E");
-        Transform pontoEsquerda = haste.transform.Find("PontoEsquerda");
-        pratoEsquerda.transform.SetParent(suporteEsquerda, true);
-        suporteEsquerda.parent = pontoEsquerda;
+        // Conectar prato esquerda ao seu suporte e ponto na haste
+        pratoEsquerda.transform.SetParent(suporteConectadoEsquerda.transform, false);
+        suporteConectadoEsquerda.transform.SetParent(pontoEsquerda.transform, true);
 
-        // Conectar o Prato Direita ao seu respectivo suporte e ponto na haste
-        Transform suporteDireita = transform.Find("Y(Prato Direita)/suporteConectado_D");
-        Transform pontoDireita = haste.transform.Find("PontoDireita");
-        pratoDireita.transform.SetParent(suporteDireita, true);
-        suporteDireita.parent = pontoDireita;
+        // Conectar prato direita ao seu suporte e ponto na haste
+        pratoDireita.transform.SetParent(suporteConectadoDireita.transform, false);
+        suporteConectadoDireita.transform.SetParent(pontoDireita.transform, true);
 
-        // Conectar a haste ao suporte
-        FixedJoint fixedHaste = haste.AddComponent<FixedJoint>();
-        fixedHaste.connectedBody = meioSuporte.GetComponent<Rigidbody>();
+        // Conectar o prato esquerdo ao ponto esquerdo
+        FixedJoint fixedJointPratoEsquerda = pratoEsquerda.AddComponent<FixedJoint>();
+        fixedJointPratoEsquerda.connectedBody = pontoEsquerda.GetComponent<Rigidbody>();
 
-        // Conectar os suportes dos pratos às extremidades da haste
-        ConnectSupportToHaste(suporteEsquerda.gameObject, haste.transform.Find("PontoEsquerda").gameObject);
-        ConnectSupportToHaste(suporteDireita.gameObject, haste.transform.Find("PontoDireita").gameObject);
+        // Conectar o prato direito ao ponto direito
+        FixedJoint fixedJointPratoDireita = pratoDireita.AddComponent<FixedJoint>();
+        fixedJointPratoDireita.connectedBody = pontoDireita.GetComponent<Rigidbody>();
     }
 
-    void ConnectSupportToHaste(GameObject support, GameObject point)
+
+    void ConectarHaste()
     {
-        FixedJoint fixedJoint = support.AddComponent<FixedJoint>();
-        fixedJoint.connectedBody = point.GetComponent<Rigidbody>();
+        // Conectar o meio da haste ao meio do suporte
+        FixedJoint fixedJoint = meioHaste.AddComponent<FixedJoint>();
+        fixedJoint.connectedBody = meioSuporte.GetComponent<Rigidbody>();
     }
-
-
 
     void AdicionarJoints()
     {
-        // Adicionar hinge joint ao Prato Esquerda
-        HingeJoint hingePratoEsquerda = pratoEsquerda.AddComponent<HingeJoint>();
+        // Adicionar hinge joint ao prato esquerda
+        hingePratoEsquerda = pratoEsquerda.AddComponent<HingeJoint>();
         hingePratoEsquerda.connectedBody = pratoEsquerda.transform.parent.GetComponent<Rigidbody>();
-        hingePratoEsquerda.useSpring = true; // Adicionando mola para tornar a junta mais rígida
-        hingePratoEsquerda.spring = new JointSpring { spring = 5000f }; // Ajuste a rigidez da mola conforme necessário
+        hingePratoEsquerda.useSpring = true;
+        hingePratoEsquerda.spring = new JointSpring { spring = 0f };
 
-        // Adicionar hinge joint ao Prato Direita
-        HingeJoint hingePratoDireita = pratoDireita.AddComponent<HingeJoint>();
+        // Adicionar hinge joint ao prato direita
+        hingePratoDireita = pratoDireita.AddComponent<HingeJoint>();
         hingePratoDireita.connectedBody = pratoDireita.transform.parent.GetComponent<Rigidbody>();
-        hingePratoDireita.useSpring = true; // Adicionando mola para tornar a junta mais rígida
-        hingePratoDireita.spring = new JointSpring { spring = 5000f }; // Ajuste a rigidez da mola conforme necessário
+        hingePratoDireita.useSpring = true;
+        hingePratoDireita.spring = new JointSpring { spring = 0f };
 
         // Adicionar fixed joint ao suporte esquerdo
-        FixedJoint fixedSuporteEsquerdo = pratoEsquerda.transform.parent.gameObject.AddComponent<FixedJoint>();
-        fixedSuporteEsquerdo.connectedBody = pratoEsquerda.transform.parent.parent.GetComponent<Rigidbody>();
+        fixedSuporteEsquerdo = suporteConectadoEsquerda.AddComponent<FixedJoint>();
+        fixedSuporteEsquerdo.connectedBody = suporteConectadoEsquerda.transform.parent.GetComponent<Rigidbody>();
 
         // Adicionar fixed joint ao suporte direito
-        FixedJoint fixedSuporteDireito = pratoDireita.transform.parent.gameObject.AddComponent<FixedJoint>();
-        fixedSuporteDireito.connectedBody = pratoDireita.transform.parent.parent.GetComponent<Rigidbody>();
+        fixedSuporteDireito = suporteConectadoDireita.AddComponent<FixedJoint>();
+        fixedSuporteDireito.connectedBody = suporteConectadoDireita.transform.parent.GetComponent<Rigidbody>();
+
+        // Configurar hinge joint da haste
+        hingeHaste = haste.AddComponent<HingeJoint>();
+        hingeHaste.connectedBody = meioHaste.GetComponent<Rigidbody>();
+        hingeHaste.anchor = Vector3.zero;
+        hingeHaste.axis = Vector3.up;
+        hingeHaste.useSpring = true;
+        hingeHaste.spring = new JointSpring { spring = 0f, damper = 0f, targetPosition = 0f };
+        hingeHaste.limits = new JointLimits { min = -90f, max = 90f };
+    }
+
+    void Update()
+    {
+        // Calcular a diferença de peso entre os pratos
+        float pesoPratoEsquerda = pratoEsquerda.GetComponent<PlacaBalanca>().pesoPrato;
+        float pesoPratoDireita = pratoDireita.GetComponent<PlacaBalanca>().pesoPrato;
+        float diferencaPeso = Mathf.Abs(pesoPratoEsquerda - pesoPratoDireita);
+
+        // Usar a diferença de peso para ajustar o ângulo da haste
+        float proporcao = Mathf.Clamp01(diferencaPeso / pratoEsquerda.GetComponent<PlacaBalanca>().pesoAtivarMax);
+        float angulo = Mathf.Lerp(-90f, 90f, proporcao);
+        JointSpring spring = hingeHaste.spring;
+        spring.targetPosition = angulo;
+        hingeHaste.spring = spring;
     }
 }
--- Assets/Scripts/PlacaBalanca.cs
+++ Assets/Scripts/PlacaBalanca.cs
@@ -1,50 +1,65 @@
-using JetBrains.Annotations;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlacaBalanca : MonoBehaviour
 {
-    public float peso { get; private set; } = 0f;
+    public float pesoPrato { get; private set; } = 0f;
     public float pesoAtivarMin = 0f;
     public float pesoAtivarMax = 0f;
     public GameObject ativar;
 
     private void OnTriggerEnter(Collider other)
     {
-        Rigidbody otherRigidbody = other.GetComponent<Rigidbody>();
+        Rigidbody otherRigidbody = other.attachedRigidbody;
         if (otherRigidbody != null)
         {
-            AdicionarPeso(otherRigidbody.mass);
+            if (other.transform.IsChildOf(transform.Find("X(Prato Esquerda)")))
+            {
+                AdicionarPesoPrato(otherRigidbody.mass);
+            }
+            else if (other.transform.IsChildOf(transform.Find("Y(Prato Direita)")))
+            {
+                AdicionarPesoPrato(otherRigidbody.mass);
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        Rigidbody otherRigidbody = other.GetComponent<Rigidbody>();
+        Rigidbody otherRigidbody = other.attachedRigidbody;
         if (otherRigidbody != null)
         {
-            RemoverPeso(otherRigidbody.mass);
+            if (other.transform.IsChildOf(transform.Find("X(Prato Esquerda)")))
+            {
+                RemoverPesoPrato(otherRigidbody.mass);
+            }
+            else if (other.transform.IsChildOf(transform.Find("Y(Prato Direita)")))
+            {
+                RemoverPesoPrato(otherRigidbody.mass);
+            }
         }
     }
 
-    private void AdicionarPeso(float massa)
+    private void AdicionarPesoPrato(float massa)
     {
-        peso += massa;
+        pesoPrato += massa;
         VerificarAtivacao();
     }
 
-    private void RemoverPeso(float massa)
+    private void RemoverPesoPrato(float massa)
     {
-        peso -= massa;
+        pesoPrato -= massa;
         VerificarAtivacao();
     }
 
     private void VerificarAtivacao()
     {
-        if (peso < pesoAtivarMin || peso > pesoAtivarMax)
+        if (pesoPrato < pesoAtivarMin || pesoPrato > pesoAtivarMax)
         {
             ativar.SetActive(true);
+        }
+        else
+        {
+            ativar.SetActive(false); // Desativar se estiver dentro do intervalo
         }
     }
 }
