0302fca45d28439b38eed4f03fd61ad0e80520d1
Humberto Corrêa Gomes <humberto.gomes002@edu.pucrs.br>
2024-03-07 11:16:11 -0300

    changes on script scale

--- Assets/Scripts/BalancaCompleta.cs
+++ Assets/Scripts/BalancaCompleta.cs
@@ -12,74 +12,48 @@ public class BalancaCompleta : MonoBehaviour
     public GameObject pontoEsquerda;
     public GameObject pontoDireita;
 
-    private HingeJoint hingePratoEsquerda;
-    private HingeJoint hingePratoDireita;
-    private FixedJoint fixedSuporteEsquerdo;
-    private FixedJoint fixedSuporteDireito;
     private HingeJoint hingeHaste;
 
     void Start()
     {
-        // Conectar os pratos aos seus respectivos suportes e pontos na haste
         ConectarPratos();
-
-        // Conectar a haste ao meio suporte
         ConectarHaste();
-
-        // Adicionar as joints aos pratos e suportes
-        AdicionarJoints();
+        AdicionarJointHaste();
     }
 
     void ConectarPratos()
     {
-        // Conectar prato esquerda ao seu suporte e ponto na haste
         pratoEsquerda.transform.SetParent(suporteConectadoEsquerda.transform, false);
         suporteConectadoEsquerda.transform.SetParent(pontoEsquerda.transform, true);
 
-        // Conectar prato direita ao seu suporte e ponto na haste
         pratoDireita.transform.SetParent(suporteConectadoDireita.transform, false);
         suporteConectadoDireita.transform.SetParent(pontoDireita.transform, true);
 
-        // Conectar o prato esquerdo ao ponto esquerdo
-        FixedJoint fixedJointPratoEsquerda = pratoEsquerda.AddComponent<FixedJoint>();
-        fixedJointPratoEsquerda.connectedBody = pontoEsquerda.GetComponent<Rigidbody>();
+        FixedJoint fixedJointPontoEsquerdaSuporteEsquerda = pontoEsquerda.AddComponent<FixedJoint>();
+        fixedJointPontoEsquerdaSuporteEsquerda.connectedBody = suporteConectadoEsquerda.GetComponent<Rigidbody>();
 
-        // Conectar o prato direito ao ponto direito
-        FixedJoint fixedJointPratoDireita = pratoDireita.AddComponent<FixedJoint>();
-        fixedJointPratoDireita.connectedBody = pontoDireita.GetComponent<Rigidbody>();
+        FixedJoint fixedJointPontoDireitaSuporteDireita = pontoDireita.AddComponent<FixedJoint>();
+        fixedJointPontoDireitaSuporteDireita.connectedBody = suporteConectadoDireita.GetComponent<Rigidbody>();
     }
-
 
     void ConectarHaste()
     {
-        // Conectar o meio da haste ao meio do suporte
-        FixedJoint fixedJoint = meioHaste.AddComponent<FixedJoint>();
-        fixedJoint.connectedBody = meioSuporte.GetComponent<Rigidbody>();
+        // Remover a FixedJoint da haste do meio
+        Destroy(meioHaste.GetComponent<FixedJoint>());
+
+        // Adicionar HingeJoint no meio da haste
+        hingeHaste = meioHaste.AddComponent<HingeJoint>();
+        hingeHaste.connectedBody = meioSuporte.GetComponent<Rigidbody>();
+        hingeHaste.anchor = Vector3.zero;
+        hingeHaste.axis = Vector3.up;
+        hingeHaste.useSpring = true;
+        hingeHaste.spring = new JointSpring { spring = 0f, damper = 0f, targetPosition = 0f };
+        hingeHaste.limits = new JointLimits { min = -90f, max = 90f };
     }
 
-    void AdicionarJoints()
+    void AdicionarJointHaste()
     {
-        // Adicionar hinge joint ao prato esquerda
-        hingePratoEsquerda = pratoEsquerda.AddComponent<HingeJoint>();
-        hingePratoEsquerda.connectedBody = pratoEsquerda.transform.parent.GetComponent<Rigidbody>();
-        hingePratoEsquerda.useSpring = true;
-        hingePratoEsquerda.spring = new JointSpring { spring = 0f };
-
-        // Adicionar hinge joint ao prato direita
-        hingePratoDireita = pratoDireita.AddComponent<HingeJoint>();
-        hingePratoDireita.connectedBody = pratoDireita.transform.parent.GetComponent<Rigidbody>();
-        hingePratoDireita.useSpring = true;
-        hingePratoDireita.spring = new JointSpring { spring = 0f };
-
-        // Adicionar fixed joint ao suporte esquerdo
-        fixedSuporteEsquerdo = suporteConectadoEsquerda.AddComponent<FixedJoint>();
-        fixedSuporteEsquerdo.connectedBody = suporteConectadoEsquerda.transform.parent.GetComponent<Rigidbody>();
-
-        // Adicionar fixed joint ao suporte direito
-        fixedSuporteDireito = suporteConectadoDireita.AddComponent<FixedJoint>();
-        fixedSuporteDireito.connectedBody = suporteConectadoDireita.transform.parent.GetComponent<Rigidbody>();
-
-        // Configurar hinge joint da haste
+        // Adicionar hinge joint da haste
         hingeHaste = haste.AddComponent<HingeJoint>();
         hingeHaste.connectedBody = meioHaste.GetComponent<Rigidbody>();
         hingeHaste.anchor = Vector3.zero;
