9a9217262088913f5fd38f88f57dc67e56eff352
Tainara Chaves <127336684+atainarachaves@users.noreply.github.com>
2024-03-11 17:05:36 -0300

    ajustes

--- Assets/Scripts/FractionManeger.cs
+++ Assets/Scripts/FractionManeger.cs
@@ -1,25 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class FractionManeger : MonoBehaviour
 {
-    
- public void Metodo() {
-        Debug.Log("teste");
-
-
-    }
-
-    // Start is called before the first frame update
+ 
+ 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
+    
     void Update()
     {
         
     }
+
 }
+
+
+
+
+
+
+   /*
+ public void Metodo() {
+        Debug.Log("teste");
+
+
+    }
+*/ 
+    
+
