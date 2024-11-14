27ae912f759158044da33eeaff566b7a81d18892
Pedro Dallarosa <pedrosd9999@gmail.com>
2024-11-14 07:41:49 -0300

    Base 10

--- Assets/Scripts/Base10/checagem.cs
+++ Assets/Scripts/Base10/checagem.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class checagem : MonoBehaviour
 {
 
-    public int unidade, dezena, centena;
-    public Collider unidadeCol, dezenaCol, centenaCol;
+    public int unidade, dezena, centena, contador;
+    public Collider unidadeCollider, dezenaCollider, centenaCollider;
+    public TMP_Text unidadeDisplay, dezenaDisplay, centenaDisplay;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        contador = 0;
     }
 
     // Update is called once per frame
@@ -23,11 +25,100 @@ public class checagem : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
 
+        if (other == unidadeCollider)
+        {
+            incUnidade();
+            unidadeDisplay.text = ("U\n" + unidade);
+            Debug.Log("Unidade Incrementada: " + unidade);
+        }
+        else if (other == dezenaCollider) 
+        {
+            incDezena();
+            dezenaDisplay.text = ("U\n" + dezena);
+            Debug.Log("Dezena Incrementada: " + dezena);
+        }
+        else
+        {
+            incCentena();
+            centenaDisplay.text = ("U\n" + centena);
+            Debug.Log("Centena Incrementada: " + centena);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        if (other == unidadeCollider)
+        {
+            decUnidade();
+            unidadeDisplay.text = ("U\n" + unidade);
+            Debug.Log("Unidade Incrementada: " + unidade);
+        }
+        else if (other == dezenaCollider)
+        {
+            decDezena();
+            dezenaDisplay.text = ("U\n" + dezena);
+            Debug.Log("Dezena Incrementada: " + dezena);
+        }
+        else
+        {
+            decCentena();
+            centenaDisplay.text = ("U\n" + centena);
+            Debug.Log("Centena Incrementada: " + centena);
+        }
+    }
 
+    void incUnidade()
+    {
+        if(unidade < 10)
+        {
+            unidade++;
+            Debug.Log("Unidade Incrementada");
+        }
+    }
+
+    void decUnidade()
+    {
+        if (unidade >= 0)
+        {
+            unidade--;
+            Debug.Log("Unidade Decrementada");
+        }
+    }
+
+    void incDezena()
+    {
+        if (dezena <= 10)
+        {
+            dezena++;
+            Debug.Log("Dezena Incrementada");
+        }
+    }
+
+    void decDezena()
+    {
+        if (dezena >= 0)
+        {
+            dezena--;
+            Debug.Log("Dezena Decrementada");
+        }
+    }
+
+    void incCentena()
+    {
+        if (centena <= 10)
+        {
+            centena++;
+            Debug.Log("Centena Incrementada");
+        }
+    }
+
+    void decCentena() 
+    {
+        if (centena >= 0)
+        {
+            centena--;
+            Debug.Log("Centena Decrementada");
+        }
     }
 
     //public bool checagemResposta()
